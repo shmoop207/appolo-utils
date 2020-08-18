98d818f1d25df3b7e0215f1f50d14907fd18c45d
roman <shmoop207@gmail.com>
2020-08-18 11:57:27 +0300

    added hash md5

--- lib/hash.ts
+++ lib/hash.ts
@@ -4,10 +4,22 @@ import {createHash, HexBase64Latin1Encoding} from "crypto";
 
 export class Hash {
     public static hashPlainObject(obj: { [index: string]: string | number | boolean }, algorithm: string = 'sha1', encoding: HexBase64Latin1Encoding = 'hex'): string {
-        return createHash(algorithm).update(Strings.stringifyObjectValues(obj)).digest(encoding);
+        return Hash.hash(Strings.stringifyObjectValues(obj));
     }
 
-    public static hashCode(s: string,prefix:string = "_"): string {
+    public static hash(str: string, algorithm: string = 'sha1', encoding: HexBase64Latin1Encoding = 'hex'): string {
+        return createHash(algorithm).update(str).digest(encoding)
+    }
+
+    public static md5(str: string): string {
+        return Hash.hash(str, "md5");
+    }
+
+    public static sha1(str: string): string {
+        return Hash.hash(str, "sha1");
+    }
+
+    public static hashCode(s: string, prefix: string = "_"): string {
         let h = 0, l = s.length, i = 0;
         if (l > 0)
             while (i < l)
