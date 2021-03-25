1adeea6d9fc02a5e0a1ab8036e77f6e1d7ba076b
roman <shmoop207@gmail.com>
2021-03-25 16:14:49 +0200

    added strings slugify

--- lib/strings.ts
+++ lib/strings.ts
@@ -42,6 +42,19 @@ export class Strings {
         return fromString;
     }
 
+    public static slugify(text: string): string {
+        text = (text || "").toString()
+            .toLowerCase()
+            .replace(/\s+/g, "-") // Replace spaces with -
+            .replace(/&/g, "-and-") // Replace & with 'and'
+            .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+            .replace(/\--+/g, "-") // Replace multiple - with single -
+            .replace(/^-+/, "") // Trim - from start of text
+            .replace(/-+$/, "") // Trim - from end of text
+
+        return text
+    }
+
     public capitalize(str: string) {
         (str || "").replace(/^\w/, (c) => c.toUpperCase());
     }
